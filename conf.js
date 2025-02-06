af8183cb37c7f4122d54b6394af3d41da294e487
Joel Denning <joeldenning@gmail.com>
2025-02-06 01:09:42 -0700

    Add angular location

--- conf.js
+++ conf.js
@@ -4,7 +4,8 @@ module.exports = {
   manifestFormat: "importmap",
   locations: {
     react: process.env.REACT_IMPORT_MAP_URL,
-    vue: process.env.VUE_IMPORT_MAP_URL
+    vue: process.env.VUE_IMPORT_MAP_URL,
+    angular: process.env.ANGULAR_IMPORT_MAP_URL,
   },
   cacheControl: "no-store",
 };
